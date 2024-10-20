fa5bb3d4acd9e241b904bee0e4800833f8b8face
Sooope <kindraddish@gmail.com>
2024-10-20 15:08:26 +0800

    simplify code

--- crawler.py
+++ crawler.py
@@ -2,34 +2,32 @@ import json
 import requests
 import curlify
 
-url = #TARGET#
+url = "--target--"
 
 
 #DO NOT CHANGE THIS PART
 prox = {'http': "socks5://127.0.0.1:9050"}
 rcookie = requests.get(url, proxies=prox)
 cookies = {"PHPSESSID":rcookie.cookies['PHPSESSID']}
-headers = {
-    'User-Agent': 'Nigger',
-}
-data = {
-    'input_unique_id': 'a',
-    'input_pass': 'a',
-    'btn_login': 'Submit',
-}
 #DO NOT CHANGE THIS PART
 
-
-'''
-data['input_unique_id'] = '2020001'
+injection = "a"
 response = requests.post(
     'https://aos.ssc.edu.hk/aos/e-sportsday/student/index.php',
     cookies=cookies,
-    headers=headers,
-    data=data,
+    data={
+    'input_unique_id': injection,
+    'input_pass': 'a',
+    'btn_login': 'Submit',
+	},
     verify=False,
     proxies=prox
 )
-'''
+
+
+
+
+
+
 print("Response content:")
 print(response.text)
