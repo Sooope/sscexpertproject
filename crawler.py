00911b98da08ef1dbfd0cb0841442711c6a0d3ab
Sooope <kindraddish@gmail.com>
2024-10-20 14:55:36 +0800

    request and payload

--- crawler.py
+++ crawler.py
@@ -1,13 +1,35 @@
 import json
 import requests
+import curlify
 
-url = "https://aos.ssc.edu.hk/aos/e-sportsday/student/index.php"
-payload = {"input_unique_id": "a", "input_pass": "a", "btn_login": "Submit"}
-prox = {'https': "socks5://127.0.0.1:9050"}
-r = requests.get(url, params=payload, proxies=prox)
-pretty = str(r.content).replace("\\r", "\r").replace("\\t", "\t").replace("\\n", "\n").replace("\\'", "\'")
-print(pretty)
+url = #TARGET#
 
 
+#DO NOT CHANGE THIS PART
+prox = {'http': "socks5://127.0.0.1:9050"}
+rcookie = requests.get(url, proxies=prox)
+cookies = {"PHPSESSID":rcookie.cookies['PHPSESSID']}
+headers = {
+    'User-Agent': 'Nigger',
+}
+data = {
+    'input_unique_id': 'a',
+    'input_pass': 'a',
+    'btn_login': 'Submit',
+}
+#DO NOT CHANGE THIS PART
 
 
+'''
+data['input_unique_id'] = '2020001'
+response = requests.post(
+    'https://aos.ssc.edu.hk/aos/e-sportsday/student/index.php',
+    cookies=cookies,
+    headers=headers,
+    data=data,
+    verify=False,
+    proxies=prox
+)
+'''
+print("Response content:")
+print(response.text)
