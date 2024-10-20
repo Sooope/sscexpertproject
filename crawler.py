b9f0af4613cc690e00d653f90167b6c4ed63c6ad
Sooope <kindraddish@gmail.com>
2024-10-20 11:37:35 +0800

    pretty output

--- crawler.py
+++ crawler.py
@@ -5,7 +5,8 @@ url = "https://aos.ssc.edu.hk/aos/e-sportsday/student/index.php"
 payload = {"input_unique_id": "a", "input_pass": "a", "btn_login": "Submit"}
 prox = {'https': "socks5://127.0.0.1:9050"}
 r = requests.get(url, params=payload, proxies=prox)
-print(str(r.content))
+pretty = str(r.content).replace("\\r", "\r").replace("\\t", "\t").replace("\\n", "\n")
+print(pretty)
 
 
 
